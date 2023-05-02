760d7696f4a60295b7dc164d1547f09a2e179ef6
IV0100730 <taekung7.kim@hyundai-di.com>
2023-05-02 17:44:09 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="app">
 		<nav>
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
+			<router-link to="/">test</router-link> |
+			<router-link to="/about">test2</router-link>
 		</nav>
 		<router-view />
 	</div>
